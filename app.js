b2ae6a222dc5a262a6b8b4d37b16e7d29d49673b
Diego-S-G <diego07goncalves@gmail.com>
2025-01-02 09:37:35 -0300

    Continuing Styling of the Task Component.

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ var app = new Vue({
         'task': {
             props: ['task'],
             template: `<div class="ui segment task" v-bind:class="task.completed ? 'done' : 'todo'">
-                            {{task.id}} {{task.name}}: {{task.description}} completed? {{task.completed}}
+                            <div class="ui grid">
+                                <div class="left floated twelve wide column">
+                                    <div class="ui checkbox">
+                                        <input type="checkbox" name="task" :checked="task.completed">
+                                        <label>{{ task.name }} <span class="description">{{ task.description }}</span></label>
+                                    </div>
+                                </div>
+                                <div class="right floated three wide column">
+
+                                </div>
+                            </div>
                         </div>`
         }
     },
