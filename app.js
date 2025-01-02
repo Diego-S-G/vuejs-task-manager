af638592c91d2f9079f18c81c65da41e4be99cc0
Diego-S-G <diego07goncalves@gmail.com>
2025-01-02 09:10:13 -0300

    Styling with Vue Class Bindings.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = new Vue({
         tasks: [
             { id: 1, name: 'Todo 1', description: 'Go to the store', completed: true },
             { id: 2, name: 'Todo 2', description: 'Finish the tutorial', completed: true },
-            { id: 3, name: 'Todo 3', description: 'Clean the house', completed: false },
+            { id: 3, name: 'Todo 3', description: 'Clean the house', completed: true },
             { id: 4, name: 'Todo 4', description: 'Go to the gym', completed: false },
             { id: 5, name: 'Todo 5', description: 'Get some sleep', completed: false }
         ],
