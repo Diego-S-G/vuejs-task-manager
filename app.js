eaba6155180855c248ff4788dee1a2707690628d
Diego-S-G <diego07goncalves@gmail.com>
2025-01-02 10:02:43 -0300

    Continuing toggleDone and Starting Delete.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = new Vue({
                                     </div>
                                 </div>
                                 <div class="right floated three wide column">
-
+                                    <i class="icon trash red" alt="Delete" v-on:click="app.deleteTask($event, task.id)"></i>
                                 </div>
                             </div>
                         </div>`
@@ -39,8 +39,21 @@ var app = new Vue({
     methods: {
         toggleDone: function(event, id) {
             let task = this.tasks.find(item => item.id == id);
-            console.log(task);
-            // task.completed = !task.completed;
+            
+            if (task) {
+                task.completed = !task.completed;
+                console.log(`Task ${task.name} is now ${task.completed ? 'done' : 'todo'}`);
+            }
+        },
+        deleteTask: function(event, id) {
+            console.log('task deleted');
+            // let task = this.tasks.find(item => item.id == id);
+            
+            // if (task) {
+            //     let index = this.tasks.indexOf(task);
+            //     this.tasks.splice(index, 1);
+            //     console.log(`Task ${task.name} has been deleted`);
+            // }
         }
     }
 })
